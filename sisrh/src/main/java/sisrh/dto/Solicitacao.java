62fc1eba6e3510dc7c32ec8aba7cf07879410e89
Marcos Alarcon <86171565+alarconmarcos@users.noreply.github.com>
2023-03-18 16:45:45 -0300

    Atividade Formativa 3
    Atividade 3 – Listar empregados ativos e inativos
    
     Atividade 4 - Listagem de Solicitações dos empregados

--- sisrh/src/main/java/sisrh/dto/Solicitacao.java
+++ sisrh/src/main/java/sisrh/dto/Solicitacao.java
@@ -2,6 +2,12 @@ package sisrh.dto;
 
 import java.sql.Date;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Solicitacao {
 
 	private Integer id;
